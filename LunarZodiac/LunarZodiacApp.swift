0eebd675b29d065096fddeefe522dccf214ef16a
Bob Witmer <bwitmer@golden.net>
2023-07-03 14:19:25 -0400

    Initial Commit

---
+++ LunarZodiac/LunarZodiacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacApp.swift
+//  LunarZodiac
+//
+//  Created by Bob Witmer on 2023-07-03.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
